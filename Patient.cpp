a2ff7188f672306062376cdddf7d93511d71448f
patterncard <patcad@o2.pl>
2019-05-05 16:02:33 +0200

    zero height, weigth

--- Patient.cpp
+++ Patient.cpp
@@ -19,12 +19,27 @@ public:
         this->weigth = 0;
     }
 
-    Patient(int ag2, float ht2, float wh2) : age(ag2), height(ht2), weigth(wh2) {}
+    Patient(char name[20], char lastName[20], int age, float height, float weigth)
+    {
+        strcpy(this->name, name);
+        strcpy(this->lastName, lastName);
+        this->age = age;
+        this->height = height;
+        this->weigth = weigth;
+    }
+    Patient(char name[20], char lastName[20], int age)
+    {
+        strcpy(this->name, name);
+        strcpy(this->lastName, lastName);
+        this->age = age;
+        this->height = 0;
+        this->weigth = 0;
+    }
 };
 
 int main()
 {
-    Patient somebody(20, 3.3, 4.4);
+    Patient somebody("Iv", "Kiel", 20);
     std::cout << "foodAmount" << std::endl;
     std::cout << somebody.age << std::endl;
     std::cout << somebody.height << std::endl;
