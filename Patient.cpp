8b3a8681470715fdb2359630337d30ffed456d1a
patterncard <patcad@o2.pl>
2019-05-05 15:21:13 +0200

    colon, pionters

--- Patient.cpp
+++ Patient.cpp
@@ -19,24 +19,13 @@ public:
         this->weigth = 0;
     }
 
-    Patient(char name[20], char lastName[20], int age, float height, float weigth)
-    {
-        strcpy(this->name, name);
-        strcpy(this->lastName, lastName);
-        this->age = age;
-        this->height = height;
-        this->weigth = weigth;
-        if (height, weigth == 0)
-            height, weigth = 0;
-    }
+    Patient(int ag2, float ht2, float wh2) : age(ag2), height(ht2), weigth(wh2) {}
 };
 
 int main()
 {
-    Patient somebody(char Ivona, char Kielecka, int 20, float 3.3, float 4.4);
+    Patient somebody(20, 3.3, 4.4);
     std::cout << "foodAmount" << std::endl;
-    std::cout << somebody.name << std::endl;
-    std::cout << somebody.lastName << std::endl;
     std::cout << somebody.age << std::endl;
     std::cout << somebody.height << std::endl;
     std::cout << somebody.weigth << std::endl;
